e7eb5d18035605385b6457c553aa105670001aeb
Kendrick Kamal <146471489+kamaljeet1989@users.noreply.github.com>
2024-03-28 21:37:28 +0100

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+</head>
+<body>
+    <h1>kamal coffe shop</h1>
+    <p>this is a nice place to have some chat </p>
+   <div>
+   <img src="https://pixabay.com/get/g2973bbe5704fc8c1547e68e673ea1d4652322b17d73da5d1d7d025d7c032840d13dc58536f733f7f3b26b8bce5afa33c213f6194513913cbb8947c9868533ad5bd0084ad2223e0f12b055e63bdd00fd4_1280.jpg" width="350" alt="">
+   </div> 
+   <div>
+    
+   </div>
+</body>
+</html>
